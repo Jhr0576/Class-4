d0f05a19ab670b5594cdd07cf0cfd51cbd54e85f
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-04-22 19:14:13 +0000

    Next Save

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,11 +4,32 @@
 
 # Booleans
 
+# this_is_true = true
+# puts this_is_true
+# this_is_false = false
+# puts this_is_false
+
 # Boolean Expressions
+
+# puts 3 == 2
+# puts 3 != 2
+# puts 3 > 2
+# puts 3 < 2
+# always use == for equals, as = is for variable assignment. == means is this true?
 
 # If Conditional Logic
 
+# if 3 == 2
+# puts "hello!"
+# end
+
+# if 3 > 2
+# puts "hello!"
+# end
+
 # If/Else Conditional Logic
+
+
 
 # Elsif Conditional Logic
 
